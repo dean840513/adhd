f4feca9a964f2e578f78604ce2b52ecaa234fa4f
Dylan Reid <dgreid@chromium.org>
2013-04-10 17:10:40 -0700

    CRAS: audio thread: remove stream if iodev open fails.
    When adding a stream to a thread, if the device fails to open, the
    procedure is repeated on the next best device.  In this case the
    stream wasn't removed from the newly created thread and when the
    thread was destroyed an extra message was sent to the client telling
    it that the stream was removed.
    
    BUG=chromium:190557
    TEST=cause iodev open failure (by removing device) and check that the
    stream moves smoothly to the next device.
    
    Change-Id: Ic9e8c994a7e7c01937ff7b922b41eca4a8727e4f
    Signed-off-by: Dylan Reid <dgreid@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47825
    Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>
    (cherry picked from commit 8a9dee38e0096f85f32682c9eab389e45c80ca85)
    Reviewed-on: https://gerrit.chromium.org/gerrit/48332

--- cras/src/server/audio_thread.c
+++ cras/src/server/audio_thread.c
@@ -271,6 +271,7 @@ int thread_add_stream(struct audio_thread *thread,
 		rc = init_device(odev, stream);
 		if (rc < 0) {
 			syslog(LOG_ERR, "Failed to open %s", odev->info.name);
+			delete_stream(thread, stream);
 			return AUDIO_THREAD_OUTPUT_DEV_ERROR;
 		}
 	}
@@ -279,6 +280,7 @@ int thread_add_stream(struct audio_thread *thread,
 		rc = init_device(idev, stream);
 		if (rc < 0) {
 			syslog(LOG_ERR, "Failed to open %s", idev->info.name);
+			delete_stream(thread, stream);
 			return AUDIO_THREAD_INPUT_DEV_ERROR;
 		}
 	}
