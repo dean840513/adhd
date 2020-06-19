5d7f6dde92a452df65e16f174b2192d03233133c
Yu-Hsuan Hsu <yuhsuan@chromium.org>
2020-06-19 10:30:27 +0800

    CRAS: cras_board_config - Fix the incompatible pointer type
    Fix the incompatible pointer types which causes fuzzing build failure.
    
    BUG=b:158543101, b:152166577
    TEST=Build Fuzz successfully
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2251566
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Tested-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Change-Id: I7d25b51d7c58d3a5e3d4bdf194df198c6898f1d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2255980
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>

--- cras/src/server/config/cras_board_config.c
+++ cras/src/server/config/cras_board_config.c
@@ -29,7 +29,7 @@ void cras_board_config_get(const char *config_path,
 {
 	char ini_name[MAX_INI_NAME_LEN + 1];
 	char ini_key[MAX_KEY_LEN + 1];
-	char *ptr;
+	const char *ptr;
 	dictionary *ini;
 
 	board_config->default_output_buffer_size = DEFAULT_OUTPUT_BUFFER_SIZE;
