a11f91b97e01fd2414b1fb171e65967b324a01be
Cheng-Yi Chiang <cychiang@chromium.org>
2020-06-29 19:37:54 +0800

    CRAS: hfp_alsa_io: Add get_valid_frames ops
    This ops is needed for hfp_alsa_io so that it can report a correct value
    to determine the best initial callback time for a new stream.
    If there is no such ops, aio device under hfp_alsa_io will not take free
    run state into account when replying cras_iodev_get_valid_frames.
    The large difference between hw_ptr and appl_ptr will be treated as a
    severe underrun and that causes stream fail to be added.
    
    BUG=b:160126114
    TEST=Test HFP iodev on Krane, add/remove the output stream repeatedly
    while iodev is in free run.
    
    Original-Change-Id: I8b4c0f18cb1c7fd0ef08ee09a4a0bb84d15283d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2272748
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Hsinyu Chao <hychao@chromium.org>
    Change-Id: If88eb38d1bb5c5e859eb88136daca46e344f6e39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2282546
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>

--- cras/src/server/cras_alsa_io.c
+++ cras/src/server/cras_alsa_io.c
@@ -1882,8 +1882,7 @@ static void set_default_hotword_model(struct cras_iodev *iodev)
 			return;
 }
 
-static int get_valid_frames(const struct cras_iodev *odev,
-			    struct timespec *tstamp)
+static int get_valid_frames(struct cras_iodev *odev, struct timespec *tstamp)
 {
 	struct alsa_io *aio = (struct alsa_io *)odev;
 	int rc;
