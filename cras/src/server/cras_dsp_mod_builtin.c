2e2dd83f314ba94aabdb474b87e2b886afc2850e
Judy Hsiao <judyhsiao@chromium.org>
2021-06-14 00:55:01 +0800

    CRAS: dsp: fix the missing dump callback for dsp modules
    Add the missing dump callback for quad_rotation,
    invert_lr and swap_lr modules to fix the crash when
    calling `cras_test_client --dump_dsp`.
    
    BUG=b:181830373
    TEST=`cras_test_client --dump_dsp`
    
    Change-Id: Ibca902decb9584c5e4688b92667312b57014c1bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2959152
    Tested-by: Judy Hsiao <judyhsiao@chromium.org>
    Auto-Submit: Judy Hsiao <judyhsiao@chromium.org>
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Reviewed-by: Chih-Yang Hsia <paulhsia@chromium.org>
    Commit-Queue: Judy Hsiao <judyhsiao@chromium.org>
    (cherry picked from commit c7b98d9b9b190599b0a0d968c3a27c0b9ed0502e)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/adhd/+/2972303
    Reviewed-by: Judy Hsiao <judyhsiao@google.com>
    Commit-Queue: Judy Hsiao <judyhsiao@google.com>
    Tested-by: Judy Hsiao <judyhsiao@google.com>

--- cras/src/server/cras_dsp_mod_builtin.c
+++ cras/src/server/cras_dsp_mod_builtin.c
@@ -198,6 +198,7 @@ static void quad_rotation_init_module(struct dsp_module *module)
 	module->deinstantiate = &quad_rotation_deinstantiate;
 	module->free_module = &empty_free_module;
 	module->get_properties = &empty_get_properties;
+	module->dump = &empty_dump;
 }
 
 /*
@@ -248,6 +249,7 @@ static void swap_lr_init_module(struct dsp_module *module)
 	module->deinstantiate = &swap_lr_deinstantiate;
 	module->free_module = &empty_free_module;
 	module->get_properties = &empty_get_properties;
+	module->dump = &empty_dump;
 }
 
 /*
@@ -294,6 +296,7 @@ static void invert_lr_init_module(struct dsp_module *module)
 	module->deinstantiate = &invert_lr_deinstantiate;
 	module->free_module = &empty_free_module;
 	module->get_properties = &empty_get_properties;
+	module->dump = &empty_dump;
 }
 
 /*
